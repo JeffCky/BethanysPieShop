0b325f7a154fea0a626fa5afc2fd4a285b0b4bc2
JeffCky <jeffcarterky@gmail.com>
2022-08-15 15:47:23 -0400

    Added Shopping Cart

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -7,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository > ();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
@@ -18,6 +22,7 @@ builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSession();
 
 if (app.Environment.IsDevelopment())
 {
@@ -32,5 +37,7 @@ app.MapDefaultControllerRoute(); //"{controller=Home}/{action=Index}/{id?}"
 
 
 DbInitializer.Seed(app);
+
+
 
 app.Run();
